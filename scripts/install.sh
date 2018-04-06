d2535b1278a2bba70bd9b53144385f8be17842d4
Emmanuel Lacourarie <emmanuel.lacourarie@edelia.fr>
2018-04-06 16:30:48 +0200

    Cowroot + upgrade packages.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y \
+apt-get update && apt-get upgrade -y && apt-get install -y \
     libc6-dev \
     git
--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+cd /home/vagrant
 if [[ ! -d "/home/vagrant/dirtycow.github.io" ]]
 then
- cd /home/vagrant
  git clone https://github.com/dirtycow/dirtycow.github.io
 fi
+
+wget https://gist.github.com/rverton/e9d4ff65d703a9084e85fa9df083c679/raw/9b1b5053e72a58b40b28d6799cf7979c53480715/cowroot.c
