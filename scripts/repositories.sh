ab527bcbcda60066efcf67b6623cb73afddd62e0
manufrommars <dev@lacourarie.org>
2018-04-07 17:32:51 +0200

    Add firefart exploit.

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 REPOS="https://github.com/dirtycow/dirtycow.github.io https://github.com/scumjr/dirtycow-vdso"
-GISTS="https://gist.github.com/rverton/e9d4ff65d703a9084e85fa9df083c679/raw/9b1b5053e72a58b40b28d6799cf7979c53480715/cowroot.c"
+GISTS="https://gist.github.com/rverton/e9d4ff65d703a9084e85fa9df083c679/raw/9b1b5053e72a58b40b28d6799cf7979c53480715/cowroot.c
+        https://raw.githubusercontent.com/FireFart/dirtycow/master/dirty.c"
 
 cd /home/vagrant
 for REPO in ${REPOS}
