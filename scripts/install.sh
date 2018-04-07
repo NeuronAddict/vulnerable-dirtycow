d651980255a6a76c373e1fe24fba670aa89422f2
manufrommars <dev@lacourarie.org>
2018-04-07 16:03:12 +0200

    Hold kernel to vulnerable version.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get upgrade -y && apt-get install -y \
+# hold kernel to vulnerable version
+apt-mark hold linux-image-generic-lts-trusty
+apt-mark hold linux-generic-lts-trusty
+apt-mark hold linux-headers-generic-lts-trusty
+
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install -y \
+    nasm \
     libc6-dev \
     git
