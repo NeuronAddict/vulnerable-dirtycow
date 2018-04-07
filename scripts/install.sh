03c3af37f123b380dd511de85f7e2fe77528e069
manufrommars <dev@lacourarie.org>
2018-04-07 16:19:19 +0200

    Upgrade to ubuntu 14.04.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # hold kernel to vulnerable version
-apt-mark hold linux-image-generic-lts-trusty
-apt-mark hold linux-generic-lts-trusty
-apt-mark hold linux-headers-generic-lts-trusty
+apt-mark hold linux-image-generic-lts-xenial
+apt-mark hold linux-generic-lts-xenial
+apt-mark hold linux-headers-generic-lts-xenial
 
 apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install -y \
     nasm \
