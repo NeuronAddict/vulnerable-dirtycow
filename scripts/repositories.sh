35e12f88f1059b950c96f5a7efae21f7a083d0ea
manufrommars <dev@lacourarie.org>
2018-04-07 17:31:10 +0200

    Improve repo script + add vdso exploit.

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-cd /home/vagrant
-if [[ ! -d "/home/vagrant/dirtycow.github.io" ]]
-then
- git clone https://github.com/dirtycow/dirtycow.github.io
-fi
+REPOS="https://github.com/dirtycow/dirtycow.github.io https://github.com/scumjr/dirtycow-vdso"
+GISTS="https://gist.github.com/rverton/e9d4ff65d703a9084e85fa9df083c679/raw/9b1b5053e72a58b40b28d6799cf7979c53480715/cowroot.c"
 
-wget https://gist.github.com/rverton/e9d4ff65d703a9084e85fa9df083c679/raw/9b1b5053e72a58b40b28d6799cf7979c53480715/cowroot.c
+cd /home/vagrant
+for REPO in ${REPOS}
+do
+    if [[ ! -d "/home/vagrant/$(basename ${REPO})" ]]
+    then
+        git clone ${REPO}
+    fi
+done
+
+for GIST in ${GISTS}
+do
+    wget ${GIST}
+done
